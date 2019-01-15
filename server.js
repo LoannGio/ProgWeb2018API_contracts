cb5aef8541bf4c64e424bbcff7089dc13db9e861
Loann Giovannangeli <lgiovannange@emi.u-bordeaux1.fr>
2019-01-15 15:07:46 +0100

    change port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const dbUtils = require('./models/contract.js');
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 80;
 
 
 app.use(function (req, res, next) {
